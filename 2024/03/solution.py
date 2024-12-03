a38638bc3a910241d4bcf236b8b6c4768b102de0
GsteigerDominik <dominik.gsteiger@gmail.com>
2024-12-03 13:09:07 +0100

    solved 2024-03 but not really happy with the code style

--- 2024/03/solution.py
+++ 2024/03/solution.py
@@ -1,6 +1,33 @@
 import re
-from pprint import pprint
 
-with open('input.txt', 'r') as file:
-    for line in file:
-        pprint(re.findall("mul\([0-9]*,[0-9]*\)",line))
+
+def mul(lines):
+    result = 0
+    for line in lines:
+        instructions = re.findall("mul\([0-9]*,[0-9]*\)", line)
+        for instruction in instructions:
+            params = [int(s) for s in re.findall('\d+', instruction)]
+            result += params[0] * params[1]
+    return result
+
+
+def mul_do_dont(lines):
+    mul_enabled = True
+    result = 0
+    for line in lines:
+        instructions = re.findall("mul\([0-9]*,[0-9]*\)|do\(\)|don\'t\(\)", line)
+        for instruction in instructions:
+            if instruction == 'do()':
+                mul_enabled = True
+            elif instruction == "don't()":
+                mul_enabled = False
+            elif mul_enabled:
+                params = [int(s) for s in re.findall('\d+', instruction)]
+                result += params[0] * params[1]
+    return result
+
+
+with (open('input.txt', 'r') as file):
+    print(mul(file))
+with (open('input.txt', 'r') as file):
+    print(mul_do_dont(file))
